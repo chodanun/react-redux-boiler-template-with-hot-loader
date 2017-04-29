37a2343adc0723982326093370577b2aeaedde42
Chodanun Srinil <step_nilanam@hotmail.com>
2017-04-29 20:17:33 +0700

    add react-hot-loader

--- src/index.js
+++ src/index.js
@@ -2,8 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { AppContainer } from 'react-hot-loader'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root')
+  )
+}
+
+render(App)
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    ReactDOM.render(
+      <AppContainer>
+        <NextApp/>
+      </AppContainer>,
+      document.getElementById('root')
+    );
+  });
+}
